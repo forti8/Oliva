81295bfea23ba3adb9bf790d52fbbfb56c7175fe
Miguel Granville <miguelp.granville@gmail.com>
2025-08-25 18:42:12 -0300

    feat: novos metodos foram adicionados a classe sendo eles o metodo ProcurarPor e LerLinha

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -3,7 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-
+import java.io.File;
+import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
@@ -37,9 +38,10 @@ public class Arquivo {
     
                 // enquanto a linha não é nula
                 while ((Linha = BR.readLine()) != null) {
-        
-                    // retorna a linha (provisório)
-                    System.out.println(Linha);
+
+                    // inicia o parsing
+                    Separador SepararLinha = new Separador(Linha);
+                    SepararLinha.Separar();
                 }
         
                 // fecha o arquivo
@@ -55,7 +57,7 @@ public class Arquivo {
 
         // exception do arquivo não encrontrado
         catch (FileNotFoundException e) {
-            System.out.println("File not found exception");
+            System.out.println("File not found exception "+ e.getMessage());
             return 1;
         }
         
@@ -127,5 +129,87 @@ public class Arquivo {
         // retorna 0 para sucesso
         // e retorna 1 para falha
         return 0;
+    }
+
+    // retorna a primeira posição onde aparece a ocorrencia
+    public int ProcurarPor (String procura) {
+
+        // tenta um scanner
+        try {
+
+            // instância um scanner
+            Scanner scanner = new Scanner(new File(this.caminho));
+
+            // armazena a linha da atual
+            int numLinha = 0;
+
+            // enquanto tiver uma proxima linha
+            while (scanner.hasNextLine()) {
+
+                // vai para proxima linha
+                String linha = scanner.nextLine();
+                numLinha++;
+
+                // se a linha tiver o conteudo 
+                if (linha.contains(procura)) {
+                    scanner.close();
+                    return numLinha;
+                }
+            }
+
+            scanner.close();
+            return -1;
+        } 
+        
+        // caso da exception de file n found
+        catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+        }
+
+        return -1;
+    }
+
+    // ler uma linha exepecifica do arquivo
+    public String LerLinha (int LinhaDesejada) {
+        
+        // tenta File reader
+        try (FileReader FR = new FileReader(this.caminho)) {
+
+            // tenta buffer reader
+            try (BufferedReader BR = new BufferedReader(FR)) {
+
+                // armazena a linha
+                String linha;
+                int linhaAtual = 0;
+    
+                // funciona enquanto a linha não é nula ou se achar a linha
+                while ((linha = BR.readLine()) != null) {
+                    
+                    // inicia a linha
+                    linhaAtual++;
+    
+                    // verifica se é a linha desejada
+                    if (linhaAtual == LinhaDesejada) {
+                        return linha;
+                    }  
+                }
+            }
+
+            // exception de input ou output
+            catch (IOException e) {
+                System.out.println(e.toString());
+            }            
+        }
+
+        // caso da exception de file n found
+        catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+        }
+        // exception de input ou output
+        catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        return "";
     }
 }
