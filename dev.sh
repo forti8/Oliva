e5a56498a26f39ea0fca319f40144f170b00c181
Miguel Granville <miguelp.granville@gmail.com>
2025-12-18 04:12:38 -0300

    chore: adicionado a criação do sub-diretório /bin/compilador/ no ditório pai /bin/ no arquivo ddev.sh, também foi adicionado o java class dos dois sub-diretórios /src/configs/ & /src/compilador/ filhos do diretório pai /src/

--- dev.sh
+++ dev.sh
@@ -4,12 +4,14 @@ set -e
 # cria o bin
 mkdir -p bin
 cd bin
-mkdir -p configs
+mkdir -p configs compilador
 cd ..
 
 # coloca as .class no bin
 javac -d bin src/*.java
+
 # javac -d bin/configs src/configs/*.java
+# javac -d bin/compilador src/compilador/*.java
 
 # executa a index
 # "$@" passa os argumentos
