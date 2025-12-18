76c3ea826a446c8cfa5839b4d7b79f1808a654b9
Miguel Granville <miguelp.granville@gmail.com>
2025-12-18 06:41:08 -0300

    chore: Atualização na classe Arquivo, agora permitindo leitura de um arquivo

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 // classe de um arquivo
 public class Arquivo {
@@ -16,25 +17,50 @@ public class Arquivo {
     }
 
     // membro estático de leitura
-    public int Ler () throws IOException {
+    public int Ler () {
 
         // define uma instância para um file reader
-        FileReader FR = new FileReader(this.caminho);
+        // tenta o file reader
+        try (FileReader FR = new FileReader(this.caminho)) {
 
-        // define uma instância para um buffer reader
-        BufferedReader BR = new BufferedReader(FR);
-
-        // armazena a linha atual
-        String Linha;
-
-        // enquanto a linha não é nula
-        while ((Linha = BR.readLine()) != null) {
-
-            // retorna a linha (provisório)
-            System.out.println(Linha);
+            // define uma instância para um buffer reader
+            BufferedReader BR = new BufferedReader(FR);
+    
+            // tenta o buffer read
+            try (BR) {
+    
+                // armazena a linha atual
+                String Linha;
+    
+                // enquanto a linha não é nula
+                while ((Linha = BR.readLine()) != null) {
+        
+                    // retorna a linha (provisório)
+                    System.out.println(Linha);
+                }
+        
+                // fecha o arquivo
+                BR.close();
+            }
+    
+            // caso de uma exception
+            catch (IOException e) {
+                System.out.println(e.toString());
+                return 1;
+            }
         }
 
-        BR.close();
+        // exception do arquivo não encrontrado
+        catch (FileNotFoundException e) {
+            System.out.println("File not found exception");
+            return 1;
+        }
+        
+        // IOException
+        catch (IOException e) {
+            System.out.println(e.toString());
+            return 1;
+        }
 
         // retorna 0 para sucesso
         // e retorna 1 para falha
