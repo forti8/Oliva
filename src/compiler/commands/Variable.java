a7a17dec5fc2e6ee38a15aa1698115cb87eaf11e
Miguel Granville <miguelp.granville@gmail.com>
2025-12-28 04:13:29 -0300

    feat: adição do metodo put(), call() e define(). também ocorreu a adição do novo tipo "STR"

--- src/compiler/commands/Variable.java
+++ src/compiler/commands/Variable.java
@@ -4,20 +4,67 @@ import compiler.libs.memo.Memory;
 public class Variable {
 
     public enum varType {
-        INT, FLOAT, BOOL, CHAR
+        INT, FLOAT, BOOL, CHAR, STR
     }
 
     public String variableName;
     public varType variableType;
     private long memoryLocation;
    
-    public void call () {
+    public Object call () {
 
-        return;
+        Object memoValue;
+        switch (this.variableType) {
+            case STR:
+                StringBuilder sb = new StringBuilder();
+                long currentPos = this.memoryLocation;
+                char c;
+                
+                while ((c = Memory.ReadChar(currentPos)) != '\0') {
+                    sb.append(c);
+                    currentPos++;
+                }
+
+                memoValue = sb.toString();
+            break;
+
+            case INT:
+                memoValue = Memory.ReadInt(this.memoryLocation);
+            break;
+
+            case FLOAT:
+                memoValue = Memory.ReadFloat(this.memoryLocation);
+            break;
+
+            case CHAR:
+                memoValue = Memory.ReadChar(this.memoryLocation);
+            break;
+
+            case BOOL:
+                memoValue = Memory.ReadBool(this.memoryLocation);
+            break;
+
+            default:
+                memoValue = null;
+        }
+
+        return memoValue;
     } 
 
     public void define (Object value) {
         switch (this.variableType) {
+            case STR:
+                String str = (String) value;
+                byte[] bytes = str.getBytes();
+
+                this.memoryLocation = Memory.Alloc(bytes.length + 1);
+                for (int i = 0; i < bytes.length; i++) {
+                    Memory.WriteChar(this.memoryLocation + i, (char) bytes[i]);
+                }
+
+                Memory.WriteChar(this.memoryLocation + bytes.length, '\0');
+            break;
+
             case INT:
                 this.memoryLocation = Memory.Alloc(4);
                 Memory.WriteInt(this.memoryLocation, (int) value);
@@ -40,16 +87,49 @@ public class Variable {
 
             default:
         }
-
-        
         return;
     }
 
-    public void put () {
+    public void put (Object value) {
+        switch (this.variableType) {
+            case STR:
+                Memory.Free(this.memoryLocation);
+                String str = (String) value;
+                byte[] bytes = str.getBytes();
+
+                this.memoryLocation = Memory.Alloc(bytes.length + 1);
+                for (int i = 0; i < bytes.length; i++) {
+                    Memory.WriteChar(this.memoryLocation + i, (char) bytes[i]);
+                }
+
+                Memory.WriteChar(this.memoryLocation + bytes.length, '\0');
+            break;
+
+            case INT:
+                Memory.WriteInt(this.memoryLocation, (int) value);
+            break;
+
+            case FLOAT:
+                Memory.WriteFloat(this.memoryLocation, (float) value);
+            break;
+
+            case CHAR:
+                Memory.WriteChar(this.memoryLocation, (char) value);
+            break;
+
+            case BOOL:
+                Memory.WriteBool(this.memoryLocation, (boolean) value);
+            break;
+
+
+            default:
+        }
         return;
     }
 
     public void delete () {
+        Memory.Free(this.memoryLocation);
+        this.memoryLocation = -1L;
         return;
     }
 
