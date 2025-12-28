9a5f562ed91b84e1ae6a1b8df65bab579b673d1c
Miguel Granville <miguelp.granville@gmail.com>
2025-12-28 04:09:20 -0300

    feat: atualização de nomes da libmemo

--- src/compiler/libs/memo/c/write/writeBool.c
+++ src/compiler/libs/memo/c/write/writeBool.c
@@ -1,6 +1,6 @@
 #include "../../include/Memory.h"
 
-JNIEXPORT jint JNICALL Java_compiler_libs_memo_Memory_nativeWriteBoolean (JNIEnv *env, jobject obj, jlong loc, jboolean val) {
+JNIEXPORT jint JNICALL Java_compiler_libs_memo_Memory_nativeWriteBool (JNIEnv *env, jobject obj, jlong loc, jboolean val) {
     jboolean * ptr = (jboolean*) loc; 
     *ptr= val;
 
